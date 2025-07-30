2eff98c5bc59f3a6f08aff524cb43983e559adb9
Harshvardhan Maskara <hvmaskara2004@gmail.com>
2025-07-30 18:42:19 +0000

    training arg

--- verifiers/fine-tuning/cpq_tool.py
+++ verifiers/fine-tuning/cpq_tool.py
@@ -98,7 +98,7 @@ run_name = "cpq-grpo_" + model_name.split("/")[-1].lower()
 training_args = vf.grpo_defaults(run_name=run_name)
 training_args.num_iterations = 2
 training_args.per_device_train_batch_size = 4
-training_args.num_generations = 5
+training_args.num_generations = 8
 training_args.gradient_accumulation_steps = 2
 training_args.max_length = 4096
 training_args.learning_rate = 2e-5
